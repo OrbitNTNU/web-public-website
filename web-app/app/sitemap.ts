929fb1d26328716f6880e29f55f2984302c2c262
August Solli Middelkoop <august.kampen@gmail.com>
2025-10-31 18:36:39 +0100

    Sitemap added.

---
+++ web-app/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        { url: "https://orbitntnu.com", lastModified: new Date() },
+        { url: "https://orbitntnu.com/about", lastModified: new Date() },
+        { url: "https://orbitntnu.com/projects", lastModified: new Date() },
+        { url: "https://orbitntnu.com/sponsors", lastModified: new Date() },
+        { url: "https://orbitntnu.com/contact", lastModified: new Date() },
+        { url: "https://orbitntnu.com/team", lastModified: new Date() },
+        { url: "https://orbitntnu.com/articles", lastModified: new Date() },
+        { url: "https://orbitntnu.com/join", lastModified: new Date() }
+    ];
+}
