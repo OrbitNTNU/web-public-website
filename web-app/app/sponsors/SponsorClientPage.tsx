48c9cefd72dca538840af4d3e7b0d461948e6627
Magnus Giverin <magnusgiverin@icloud.com>
2025-10-31 15:56:50 +0100

    Add: Add first draft of sponsors page

--- web-app/app/sponsors/SponsorClientPage.tsx
+++ web-app/app/sponsors/SponsorClientPage.tsx
@@ -8,32 +8,53 @@ import BronzeSponsors from "@/components/SponsorsPage/BronzeSponsors";
 import PartnerSponsors from "@/components/SponsorsPage/PartnerSponsors";
 import GoldSponsors from "@/components/SponsorsPage/GoldSponsors";
 import SilverSponsors from "@/components/SponsorsPage/SilverSponsors";
+import AllThanks from "@/components/SponsorsPage/AllThanks";
 
 export default function SponsorClientPage({
   sponsorsPage,
 }: {
   sponsorsPage: SponsorsPage;
 }) {
+
+  const mainSponsors = sponsorsPage.mainSponsors || [];
+  const platinumSponsors = sponsorsPage.platinumSponsors || [];
+  const goldSponsors = sponsorsPage.goldSponsors || [];
+  const silverSponsors = sponsorsPage.silverSponsors || [];
+  const bronzeSponsors = sponsorsPage.bronzeSponsors || [];
+  const partnerSponsors = sponsorsPage.partners || [];
+
+  const allSponsors = [
+    ...mainSponsors,
+    ...platinumSponsors,
+    ...goldSponsors,
+    ...silverSponsors,
+    ...bronzeSponsors,
+    ...partnerSponsors,
+  ];
+
   return (
     <div className="w-full relative max-w-[1600px] mx-auto gap-20 md:gap-40 my-20 md:my-40 flex flex-col">
-      {sponsorsPage.mainSponsors && (
-        <MainSponsors sponsors={sponsorsPage.mainSponsors} />
+      {mainSponsors && (
+        <MainSponsors sponsors={mainSponsors} />
       )}
-      {sponsorsPage.platinumSponsors && (
-        <PlatinumSponsors sponsors={sponsorsPage.platinumSponsors} />
+      {platinumSponsors && (
+        <PlatinumSponsors sponsors={platinumSponsors} />
       )}
-      {sponsorsPage.goldSponsors && (
-        <GoldSponsors sponsors={sponsorsPage.goldSponsors} />
+      {goldSponsors && (
+        <GoldSponsors sponsors={goldSponsors} />
       )}
-      {sponsorsPage.silverSponsors && (
-        <SilverSponsors sponsors={sponsorsPage.silverSponsors} />
+      {silverSponsors && (
+        <SilverSponsors sponsors={silverSponsors} />
       )}
-      {sponsorsPage.bronzeSponsors && (
-        <BronzeSponsors sponsors={sponsorsPage.bronzeSponsors} />
+      {bronzeSponsors && (
+        <BronzeSponsors sponsors={bronzeSponsors} />
       )}
-      {sponsorsPage.partners && (
-        <PartnerSponsors sponsors={sponsorsPage.partners} />
+      {partnerSponsors && (
+        <PartnerSponsors sponsors={partnerSponsors} />
       )}
+      <AllThanks
+        sponsors={ allSponsors }
+      />
     </div>
   );
 }
