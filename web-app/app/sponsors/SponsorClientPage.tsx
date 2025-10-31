3b29a55e4429a1cab5d08b1548704d1e9ca5d7fa
Magnus Giverin <magnusgiverin@icloud.com>
2025-10-31 15:45:22 +0100

    Remvoe: Get rid of that naaaaaasty header

--- web-app/app/sponsors/SponsorClientPage.tsx
+++ web-app/app/sponsors/SponsorClientPage.tsx
@@ -16,7 +16,6 @@ export default function SponsorClientPage({
 }) {
   return (
     <div className="w-full relative max-w-[1600px] mx-auto gap-20 md:gap-40 my-20 md:my-40 flex flex-col">
-      <Header title={sponsorsPage.title} subtitle={sponsorsPage.caption} />
       {sponsorsPage.mainSponsors && (
         <MainSponsors sponsors={sponsorsPage.mainSponsors} />
       )}
