0053a354d571d4cc1877be95536ed9e23e77f2e2
August Solli Middelkoop <august.kampen@gmail.com>
2025-10-31 18:31:26 +0100

    Metadata test

--- web-app/app/projects/[slug]/page.tsx
+++ web-app/app/projects/[slug]/page.tsx
@@ -1,6 +1,79 @@
 // app/projects/[slug]/page.tsx
 import { getBigProject } from "@/sanity/fetch/SanityFetch";
 import ProjectClientPage from "@/app/projects/ProjectClientPage";
+import {Metadata} from "next";
+export const metadata: Metadata = {
+  title: "Selfiesat | Framsat | Framsat 1.5 | Biosat",
+  description:
+      "",
+
+  keywords: [
+    "selfiesat",
+    "framsat",
+    "biosat",
+    "nextsat",
+    "sub-orbital",
+    "ORBIT",
+  ],
+
+  authors: [{ name: "ORBITNTNU", url: "https://orbitntnu.com" }],
+  creator: "ORBIT - WEB",
+  publisher: "ORBIT",
+  category: "Nonprofit",
+
+  openGraph: {
+    title: "",
+    description:
+        "",
+    url: "https://orbitntnu.com/sponsors",
+    siteName: "ORBITNTNU",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "",
+        width: 1200,
+        height: 630,
+        alt: "",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "",
+    description:
+        "",
+    creator: "@YourTwitterHandle",
+    site: "@YourTwitterHandle",
+    images: ["https://yoursite.com/og/sponsors-og-image.jpg"],
+  },
+  alternates: {
+    canonical: "https://yoursite.com/sponsors",
+    languages: {
+      "en-US": "https://yoursite.com/sponsors",
+    },
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 interface ProjectPageProps {
   params: { slug: string };
