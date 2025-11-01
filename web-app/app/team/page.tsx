a3aab396626cc55f5f4694b23f410871a513d5ec
August Solli Middelkoop <august.kampen@gmail.com>
2025-11-01 19:09:56 +0100

    Add loading

--- web-app/app/team/page.tsx
+++ web-app/app/team/page.tsx
@@ -2,10 +2,14 @@ import TeamsClientPage from "@/app/team/TeamsClientPage";
 import {getTeamsData} from "@/components/TeamsPage/lib/getTeamsData";
 import {Loading} from "@/components/Loading";
 
+
 export const revalidate = 300; //5 minutter
 
 export default async function TeamsPage() {
   const teamsData = await getTeamsData();
+  if(!teamsData){
+    <Loading/>
+  }
 
   return <TeamsClientPage initialTeamsData={teamsData} />;
 }
