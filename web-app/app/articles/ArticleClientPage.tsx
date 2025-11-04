12df347c5862a1761a857957e5d01d7a68fc6ab7
Magnus Giverin <magnusgiverin@icloud.com>
2025-11-04 19:45:29 +0100

    final final final change

--- web-app/app/articles/ArticleClientPage.tsx
+++ web-app/app/articles/ArticleClientPage.tsx
@@ -41,30 +41,28 @@ const ArticlesClientPage = ({ articles }: ArticlesClientPageProps) => {
       ))}
 
       {/* All other articles */}
-      <section>
-        <motion.h3
-          initial={{ opacity: 0, x: -50 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ type: "tween", stiffness: 200 }}
-          className="mb-8 tracking-wider"
-        >
-          Our Articles
-        </motion.h3>
-        {
-          sorted.length > 2 && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {sorted.slice(1).map((article) => (
-              <RegularArticle
-                key={article._id}
-                article={article}
-                isMobile={isMobile}
-              />
-            ))}
-          </div>
-          )
-        }
-      </section>
+      {sorted.length > 2 && (
+          <section>
+            <motion.h3
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true, amount: 0.3 }}
+              transition={{ type: "tween", stiffness: 200 }}
+              className="mb-8 tracking-wider"
+            >
+              Our Articles
+            </motion.h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+              {sorted.slice(1).map((article) => (
+                <RegularArticle
+                  key={article._id}
+                  article={article}
+                  isMobile={isMobile}
+                />
+              ))}
+            </div>
+          </section>
+      )}
     </section>
   );
 };
