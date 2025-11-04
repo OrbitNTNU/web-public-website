33b153318c5c926fded35760aaacabbd6799de3d
Magnus Giverin <magnusgiverin@icloud.com>
2025-11-04 19:44:32 +0100

    Fix articles again

--- web-app/app/articles/ArticleClientPage.tsx
+++ web-app/app/articles/ArticleClientPage.tsx
@@ -52,7 +52,7 @@ const ArticlesClientPage = ({ articles }: ArticlesClientPageProps) => {
           Our Articles
         </motion.h3>
         {
-          sorted.length > 1 && (
+          sorted.length > 2 && (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {sorted.slice(1).map((article) => (
               <RegularArticle
