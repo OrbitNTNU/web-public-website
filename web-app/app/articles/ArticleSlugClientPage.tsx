ca69cad5791fb2590a150b077c2849a0a445d239
Magnus Giverin <magnusgiverin@icloud.com>
2025-11-05 11:19:35 +0100

    Fix: Z index

--- web-app/app/articles/ArticleSlugClientPage.tsx
+++ web-app/app/articles/ArticleSlugClientPage.tsx
@@ -41,6 +41,7 @@ const ArticleSlugClientPage = ({ article }: ArticleSlugClientPageProps) => {
                         initial={{ opacity: 0, y: 20 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.5, delay: 0.2 }}
+                        className="z-10"
                     >
                         <Image
                             src={imageBuilder(article.mainImage)}
