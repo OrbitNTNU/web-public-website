911d9c159228fa3492aff19df23ddf37a00b29c9
Magnus Giverin <magnusgiverin@icloud.com>
2025-11-14 15:30:19 +0100

    Add: Add projects

--- web-app/app/projects/ProjectClientPage.tsx
+++ web-app/app/projects/ProjectClientPage.tsx
@@ -6,6 +6,10 @@ import BannerImage from "@/components/General/BannerImage";
 import { Loading } from "@/components/Loading";
 import { useNavbar } from "@/components/General/Layout/NavbarContext";
 import { useEffect } from "react";
+import LargeImage from "@/components/General/LargeImage";
+import SpanningText from "@/components/General/SpanningText";
+import DoubleImages from "@/components/General/DoubleImages";
+import LargeQuote from "@/components/General/LargeQuote";
 
 interface ProjectClientPageProps {
   project: BigProject | SubOrbitalProject;
@@ -32,12 +36,72 @@ export default function ProjectClientPage({ project }: ProjectClientPageProps) {
           switch (section._type) {
             case "bannerImage":
               return (
+                <div className="h-screen mb-20" key={section._key}>
                   <BannerImage
                       key={section._key}
                       backgroundSrc={imageBuilder(section.image.asset._ref)}
                       patchSrc={imageBuilder(project.patch)}
                       colors={project.gradientColors ?? []}
                       isBiosat={isBiosat}
+                  />
+                  </div>
+              );
+              case "largeImage":
+              return (
+                  <LargeImage
+                      key={section._key}
+                      src={imageBuilder(section.image)}
+                      alt={"Large Image"}
+                      caption={section.caption}
+                  />
+              );
+            case "spanningText":
+              return (
+                  <SpanningText
+                      key={section._key}
+                      text={section.text}
+                  />
+              );
+            case "doubleImage":
+              return (
+                  <DoubleImages
+                      key={section._key}
+                      title1={section.title1}
+                      caption1={section.caption1}
+                      src1={imageBuilder(section.image1)}
+                      alt1={section.alt1 ?? ""}
+                      title2={section.title2}
+                      caption2={section.caption2}
+                      src2={imageBuilder(section.image2)}
+                      alt2={section.alt2 ?? ""}
+                  />
+              );
+            case "doubleImageCollage":
+              return (
+                  <section key={section._key} className="flex flex-col gap-12">
+                    {section.items?.map((item) => (
+                        <DoubleImages
+                            key={item._key || Math.random().toString()}
+                            variant={item.variant}
+                            src1={imageBuilder(item.image1)}
+                            alt1={item.alt1 ?? ""}
+                            title1={item.title1}
+                            caption1={item.caption1}
+                            link1={item.link1}
+                            src2={imageBuilder(item.image2)}
+                            alt2={item.alt2 ?? ""}
+                            title2={item.title2}
+                            caption2={item.caption2}
+                            link2={item.link2}
+                        />
+                    ))}
+                  </section>
+              );
+            case "largeQuote":
+              return (
+                  <LargeQuote
+                      key={section._key}
+                      text={section.quote}
                   />
               );
             default:
