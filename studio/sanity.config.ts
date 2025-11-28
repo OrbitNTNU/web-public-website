dc6ea5ba25caaf2babfd1821caa79c14467d3764
August Solli Middelkoop <august.kampen@gmail.com>
2025-11-28 14:24:54 +0100

    Fix for visual presentation as well as draft management

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,15 +1,40 @@
-import { createClient } from "@sanity/client";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { colorInput } from "@sanity/color-input";
+import { deskStructure } from "./deskStructure";
+import { schemaTypes } from "./schemaTypes";
+import { presentationTool } from "sanity/presentation";
 
-export function getSanityClient(preview: boolean = false) {
-  return createClient({
-    projectId: "mt6p5031",
-    dataset: "production",
-    apiVersion: "2024-01-01",
-    perspective: preview ? "previewDrafts" : "published",
+export default defineConfig({
+  name: "default",
+  title: "orbitntnu-web",
 
-    token: preview ? process.env.PRESENTATION : undefined,
-    useCdn: !preview,
-  });
-}
+  projectId: "mt6p5031",
+  dataset: "production",
 
-export const client = getSanityClient(false);
+  plugins: [
+    structureTool({
+      structure: deskStructure,
+    }),
+
+    visionTool(),
+    colorInput(),
+
+    presentationTool({
+      previewUrl: {
+        origin: "https://web-public-website.vercel.app",
+        preview: "/",
+        previewMode: {
+          enable: "/api/draft",
+        },
+      },
+    }),
+
+  ],
+
+
+  schema: {
+    types: schemaTypes,
+  },
+});
