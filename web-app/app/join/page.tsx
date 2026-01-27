df70c79ed0b68635412f843a69af621546bc3299
August Solli Middelkoop <august.kampen@gmail.com>
2026-01-27 20:01:02 +0100

    Landingpage

--- web-app/app/join/page.tsx
+++ web-app/app/join/page.tsx
@@ -8,29 +8,6 @@ import { toSlug } from "@/lib/teams";
 const data = {
   title: "Join Us!",
   text: "Become a part of ORBIT and embark on an exciting journey in satellite technology. Whether you're a student eager to learn or a professional looking to contribute, we welcome you to join our team. Explore our open positions and find out how you can make a difference with ORBIT.",
-  buttons: [
-    {
-      buttonText: "View Open Positions",
-      buttonLink: "https://orbitntnu.com/join#open-positions",
-      icon: "work_outline",
-    },
-    {
-      buttonText: "Contact Us",
-      buttonLink: "mailto:contact@orbitntnu.com",
-      icon: "mail_outline",
-    },
-    {
-      buttonText: "Follow us on LinkedIn",
-      buttonLink: "https://www.linkedin.com/company/orbitntnu/",
-      icon: "link",
-    },
-    {
-      buttonText: "Learn About Orbit",
-      buttonLink: "https://orbitntnu.com/about",
-      icon: "info_outline",
-    },
-  ],
-  images: ["/tests/1.png", "/tests/2.png", "/tests/5.jpg", "/tests/6.png"],
 };
 export const revalidate = false;
 
