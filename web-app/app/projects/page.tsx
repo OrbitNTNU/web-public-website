b826f5f624c44bf1dc1dff9df11251537886f4c0
August Solli Middelkoop <august.kampen@gmail.com>
2026-01-28 17:10:51 +0100

    Cache possibly working. Revalidate 60 sec with webhook

--- web-app/app/projects/page.tsx
+++ web-app/app/projects/page.tsx
@@ -67,7 +67,7 @@ export const metadata: Metadata = {
     },
   },
 };
-export const revalidate = 60; // or any number
+export const revalidate = 60;
 
 export default async function ProjectsPage() {
   const BigProjects = await getAllBigProjects();
