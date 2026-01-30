d3c6040e4795355bccf4dca6c7687b3104eebd52
August Solli Middelkoop <august.kampen@gmail.com>
2026-01-30 20:12:58 +0100

    Sitemap modified

--- web-app/app/sitemap.ts
+++ web-app/app/sitemap.ts
@@ -2,37 +2,37 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "https://web-public-website.vercel.app/", lastModified: new Date() },
+    { url: "https://orbitntnu.com/", lastModified: new Date() },
     {
-      url: "https://web-public-website.vercel.app/about",
+      url: "https://orbitntnu.com/about",
       lastModified: new Date(),
     },
     {
-      url: "https://web-public-website.vercel.app/projects",
+      url: "https://orbitntnu.com/projects",
       lastModified: new Date(),
     },
     {
-      url: "https://web-public-website.vercel.app/sponsors",
+      url: "https://orbitntnu.com/sponsors",
       lastModified: new Date(),
     },
     {
-      url: "https://web-public-website.vercel.app/contact",
+      url: "https://orbitntnu.com/contact",
       lastModified: new Date(),
     },
     {
-      url: "https://web-public-website.vercel.app/team",
+      url: "https://orbitntnu.com/team",
       lastModified: new Date(),
     },
     {
-      url: "https://web-public-website.vercel.app/articles",
+      url: "https://orbitntnu.com/articles",
       lastModified: new Date(),
     },
     {
-      url: "https://web-public-website.vercel.app/join",
+      url: "https://orbitntnu.com/join",
       lastModified: new Date(),
     },
     {
-      url: "https://web-public-website.vercel.app/partners",
+      url: "https://orbitntnu.com/partners",
       lastModified: new Date(),
     },
   ];
